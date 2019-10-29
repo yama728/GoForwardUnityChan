39a480ae17ee6252d572e53f675f64474c945d95
natsuya yamashiro <azuki728@me.com>
2019-10-29 15:41:31 +0900

    課題です。CubeAudioというスクリプトを作成し、タグを設定した地面及びキューブそれぞれと接触する際に音が鳴るようにしました。よろしくお願い致します。

---
+++ Assets/CubeAudio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAudio : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+
+        //地面とキューブが接触した時に音を鳴らす
+        if (other.gameObject.tag == "ground")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        //キューブとキューブが接触した時に音を鳴らす
+        if (other.gameObject.tag == "block")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
